653dfe459b169e17db28e23b381bb892d5033c0f
luvk1412 <luvk1412@gmail.com>
2017-03-03 01:45:08 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -86,3 +86,35 @@ void etf(){
 		}
 	}
 }
+
+bool miller(ll n){
+	if(n <=1 || n % 2 == 0){
+		if(n != 2){
+			return false;
+		}
+	}
+	if(n == 2 || n == 3){
+		return true;
+	}
+	ll d = n-1;
+	while(d % 2 == 0){
+		d /= 2;
+	}
+	ll a[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+	for(int i = 0; i < 12 && a[i] < n; ++i){
+		ll temp = d;
+	ll mod = binexp2(a[i], temp, n);
+	if( mod == 1 || mod == n-1){
+		continue;
+	}
+	while(temp != n-1 && mod != n-1){
+		mod = multiply(mod, mod, n);
+		temp *= 2;
+	}
+	if(mod != n-1){
+		return false;
+	}  
+
+    }
+    return true;
+}
