9b8ab7689415143423bd58482df087464cd09d99
siddhant gautam <IIT2016069@iiita.ac.in>
2017-06-30 17:10:52 +0530

    template.cpp
    added nth fibonacci in log(n) . n can be upto 1e18.

--- template.cpp
+++ template.cpp
@@ -184,3 +184,25 @@ ll sum(ll i, ll bit[]){
 	}
 	return ans;
 }
+
+Nth-fibonaaci-in-log-n
+
+#define long long long
+const long M = 1000000007; // modulo
+map<long, long> F;
+long f(long n) {
+	if (F.count(n)) return F[n];
+	long k=n/2;
+	if (n%2==0) { // n=2*k
+		return F[n] = (f(k)*f(k) + f(k-1)*f(k-1)) % M;
+	} else { // n=2*k+1
+		return F[n] = (f(k)*f(k+1) + f(k-1)*f(k)) % M;
+	}
+}
+
+main(){
+	long n;
+	F[0]=F[1]=1;
+	while (cin >> n)
+	cout << (n==0 ? 0 : f(n-1)) << endl;
+}
