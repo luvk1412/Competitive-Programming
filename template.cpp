a4c067c954edc6b158309468d65b572512dbed15
Luv <luvk1412@gmail.com>
2017-06-30 21:28:30 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,28 +1,46 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define ull unsigned long long int
 #define pb push_back
 #define mp make_pair
 #define fi first
 #define se second
 #define p(x) printf("%d\n", x)
 #define pl(x) printf("%lld\n", x)
-#define p2(x, y) printf("%d %d\n", x, y)
-#define pl2(x, y) printf("%lld %lld\n", x, y)
-#define pf(x) printf("%lf\n", x)
 #define s(x) scanf("%d", &x)
 #define sl(x) scanf("%lld", &x)
 #define sf(x) scanf("%lf", &x)
-#define INF 1e18+9 
+#define INF 1e18+9
+#define endl '\n'
+#define FIO ios_base::sync_with_stdio(false)
 using namespace std;
+#define M 1000000007
+#define MAX 1000001
 
-void fs(ll &x)
-{
-    register ll c = getchar_unlocked();
-    x = 0;
-    for(; (c<48 || c>57); c = getchar_unlocked());
-    for(; c>47 && c<58; c = getchar_unlocked()){
-    	x = (x<<1) + (x<<3) + c - 48;
-    }
+time spent(){ 
+	clock_t begin = clock();
+	// CODE
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+}
+
+void ss(string &i){
+	int temp=getchar_unlocked();
+	while((temp < 'a' || temp > 'z') && (temp < 'A' || temp > 'Z'))
+		temp=getchar_unlocked();
+	while((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z')){
+		i.pb((char)temp);
+		temp = getchar_unlocked();
+	}
+}
+
+void fs(ll &x){
+	register ll c = getchar_unlocked();
+	x = 0;
+	for(; (c<48 || c>57); c = getchar_unlocked());
+	for(; c>47 && c<58; c = getchar_unlocked()){
+		x = (x<<1) + (x<<3) + c - 48;
+	}
 }
 
 bool cmp(node a, node b){
@@ -88,7 +106,7 @@ ll binexp2(ll a, ll b, ll m){
 		a %= m;
 		b = b >> 1;
 	}
-	return result;
+ d	return result;
 }
 
 void sieve(){
@@ -183,4 +201,68 @@ ll sum(ll i, ll bit[]){
 		i = (i & (i-1));
 	}
 	return ans;
+}
+
+// segmet tree
+
+void build(int a[], int v, int tl, int tr){
+	if(tl == tr){
+		t[v] = a[tl];
+	}
+	else{
+		int tm = (tl+tr) >> 1;
+		build(a, v << 1, tl, tm);
+		build(a, (v << 1)+1, tm+1, tr);
+		t[v] = t[v << 1] + t[(v << 1) + 1];
+	}
+}
+ll sum(int v, int tl, int tr, int l, int r){
+	if(l > r){
+		return 0;
+	}
+	if(tl == l && tr == r){
+		return t[v];
+	}
+	int tm = (tl + tr) >> 1;
+	return sum(v << 1, tl, tm, l, min(tm, r)) + sum((v << 1)+1, tm + 1, tr, max(l, tm+1), r);
+}
+void update(int v, int tl, int tr, int pos, int val){
+	if(tl == tr){
+		t[v] = val;
+	}
+	else{
+		int tm = (tl+tr) >> 1;
+		if(pos <= tm){
+			update(v << 1, tl, tm, pos, val);
+		}
+		else{
+			update((v << 1) + 1, tm+1, tr, pos, val);
+		}
+		t[v] = t[v << 1] + t[(v << 1)+1];
+	}
+}
+
+// DSU
+
+int parent[MAX], rnk[MAX];
+void make(int v){
+	parent[v] = v;
+	rnk[v] = 0;
+}
+
+int find(int v){
+	if(parent[v] != v)
+		parent[v] = find(parent[v]);
+	return parent[v];
+}
+void Union(int a, int b){
+	a = find(a);
+	b = find(b);
+	if(a != b){
+		if(rnk[a] < rnk[b])
+			swap(a, b);
+		parent[b] = a;
+		if(rnk[a] == rnk[b])
+			rnk[a]++;
+	}
 }
